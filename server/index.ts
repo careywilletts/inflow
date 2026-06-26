671752197aeaa49d6d85be191edb9055b03b28bd
Carey Willetts <careywilletts@Mac.communityfibre.co.uk>
2026-06-26 22:27:51 +0100

    Add CORS headers for mobile app

--- server/index.ts
+++ server/index.ts
@@ -24,6 +24,15 @@ declare module "express-session" {
 
 app.set("trust proxy", 1);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Cookie, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(
   express.json({
     verify: (req, _res, buf) => {
